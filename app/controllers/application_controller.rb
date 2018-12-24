5be4951cac02112844c051ae46cb4e937ba07be2
aliciapflaumer <alpfla7@gmail.com>
2018-12-24 14:57:19 -0500

    Created and dropped table
    - Removed `Home` table with `rails g migration dropHome`

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+
+  protected
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  # Basic password validation
+  validates_length_of :password, maximum: 20, minimum: 6, allow_nil: true,
+    allow_blank: false
+  validates_confirmation_of :password, allow_nil: true, allow_blank: false
+
+  # Make sure username is present and unique
+  validates_presence_of     :username
 end
